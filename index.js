9252e6a9696e027de48a1b25f697cf87605f4086
Shane <navysupra@gmail.com>
2022-08-08 06:09:57 +0000

    starting to implement the first bits of code

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let balance = 500.00;
 
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
 class Withdrawal {
 
   constructor(amount) {
@@ -11,19 +17,32 @@ class Withdrawal {
   }
 
 }
-
+class Deposit {
+  constructor(amount) {
+    this.amount = amount;
+  }
+  commit() {
+    balance += this.amount;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('snow-patrol');
+
+const t1 = new Withdrawal(50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
+
+const t3 = new Deposit(120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
 
 console.log('Balance:', balance);
